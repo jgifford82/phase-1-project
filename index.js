fb018b5de392c8213181355fb5b03345a41f9d42
Jennifer Gifford <jgifford82@gmail.com>
2022-07-04 12:39:31 -0600

    create preventDefault on form submit

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log("DOM fully loaded");
+    
+    const form = document.querySelector('form');
+    // console.log(form)
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log("prevented")
+    })
 });
+
+// Next: create new element on form Submit, append to the DOM, and change text content to value of form input.
+// Also clear form on submit. 
