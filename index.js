a5386562d06a89f04860bbc0a6bc4a772e8b2539
Jennifer Gifford <jgifford82@gmail.com>
2022-07-05 12:58:15 -0600

    add mouseover event listener on image

--- index.js
+++ index.js
@@ -11,19 +11,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
         // console.log(p);
         document.getElementById("form").append(p);
         // console.log(document.getElementById("name").value)
-        const userName = document.getElementById("name").value
-        p.innerText = `Greetings, ${userName}!`
+        const userName = document.getElementById("name").value;
+        p.innerText = `Greetings, ${userName}!`;
 
-        document.getElementById("name").value = ""
-    })
+        document.getElementById("name").value = "";
+    });
+
+    const img = document.querySelector('img');
+    // console.log(img)
+    img.addEventListener("mouseover", (event) => {
+        alert("Purrrr");
+    });
 
 });
 
 // Next:Pet your familiar
-// add div with id="failiar"
-// Need to find a cat image and link it
-// put familiar image of a cat (or gif?)
-// https://images.pexels.com/photos/1931371/pexels-photo-1931371.jpeg?cs=srgb&dl=pexels-helena-lopes-1931371.jpg&fm=jpg
-// set up event listener for mouse over event
-// display alert "purr" on mouse over
 // alert should disappear after a few seconds
+// add caption or p element that says "Pet your familiar"
