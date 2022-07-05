fe091b8e1fbccca0b0913c3f1850dc256f9389d4
Jennifer Gifford <jgifford82@gmail.com>
2022-07-05 12:10:17 -0600

    clear form on submit event

--- index.js
+++ index.js
@@ -13,8 +13,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
         // console.log(document.getElementById("name").value)
         const userName = document.getElementById("name").value
         p.innerText = `Greetings, ${userName}!`
+
+        document.getElementById("name").value = ""
     })
 
 });
 
-// Next: clear form on submit. 
+// Next:Pet your familiar
+// add div with id="failiar"
+// Need to find a cat image and link it
+// put familiar image of a cat (or gif?)
+// https://images.pexels.com/photos/1931371/pexels-photo-1931371.jpeg?cs=srgb&dl=pexels-helena-lopes-1931371.jpg&fm=jpg
+// set up event listener for mouse over event
+// display alert "purr" on mouse over
+// alert should disappear after a few seconds
